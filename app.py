59d79af1b891a6770fe636d2c8fc752e4996ff9e
rkstrdee <rkstrdee@gmail.com>
2020-08-25 01:06:25 +0530

    extracting links done

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import requests
+from requests.compat import urlparse
 from bs4 import BeautifulSoup
 from pymongo import MongoClient
 
@@ -22,14 +23,53 @@ collection.insert_one(website)
 internal_urls = set()
 external_urls = set()
 
-res = requests.get('https://flinkhub.com/')
+def is_valid(url):
+    try:
+        requests.get(url)
+        return True
+    except:
+        return False
 
-soup = BeautifulSoup(res.text)
-# print(soup.prettify())
-a_tags = soup.find_all('a')
-for link in a_tags:
-    print(link.get('href'))
+def get_base_url(url):
+    return urlparse(url)[0]+"://"+urlparse(url)[1]
+
+
+def get_all_links(url):
+    if url.startswith('http'):
+        base_url = get_base_url(url)
+    res = requests.get(url).content
+    soup = BeautifulSoup(res, "html.parser")
+    # print(soup.prettify())
+    a_tags = soup.find_all('a')
+
+    for link in a_tags:
+        href = link.get('href')
+        # print(href)
+        if href=="" or href is None:
+            continue
+        if href.startswith('/'):
+            local_link = base_url + href
+            internal_urls.add(local_link)
+        elif not href.startswith('http'):
+            pass
+            # local_link = path + href
+            # internal_urls.add(local_link)
+        else:
+            external_urls.add(href)
+    for link in external_urls:
+        print(link)
+    print("NOW")
+    for link in internal_urls:
+        print(link)
+    # print("NOW")
+    # print(internal_urls)
+
+
+my_url = input("Please Enter Your Address : ")
+get_all_links(my_url)
+
+
 # print(a_tags)
-# print(collection)
-for item in collection.find():
-    print(item)
+# # print(collection)
+# for item in collection.find():
+#     print(item)
